e5c9ce901b4e5fbb32febd4a02906a2c37dc755f
somprojecto-prog <somprojecto@gmail.com>
2026-09-26 00:12:43 +0100

    Delete lib directory

--- lib/toast.ts
+++
@@ -1,4 +0,0 @@
-export function showToast(message: string, duration = 2200) {
-  if (typeof window === "undefined") return;
-  window.dispatchEvent(new CustomEvent("rd-toast", { detail: { message, duration } }));
-}
